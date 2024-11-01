793a94bd49323f4560ad71e4333e4a3a5b3e907f
pang21794 <sasikan21794@gmail.com>
2024-11-01 16:32:29 +0700

    edit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database_name', 'username', 'password', {
-host: 'localhost',
+const sequelize = new Sequelize('nodedb_5tfz', 'nodedb', 'kelTlfEaeZXjxQNfPqUXSBrxefn3U9II', {
+host: 'dpg-csi9vht6l47c73f61mbg-a',
 dialect: 'postgres',
 });
 
